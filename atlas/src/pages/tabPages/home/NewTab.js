b1069262f991cbcc505655b12e426b1be7b3fdf0
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2021-03-16 15:28:15 -0400

    Feature/NewTab in home, pulling data

--- atlas/src/pages/tabPages/home/NewTab.js
+++ atlas/src/pages/tabPages/home/NewTab.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import "./NewTab.css";
+import { uploads } from './newTestData';
 
 const NewTab = () => {
     return (
         <div className="NewTabContainer">
-            <h2 id="newUploads">New Uploads</h2>
+            <div>
+                <h2 id="newUploads">New Uploads</h2>
+            </div>
+            <div className="newList">
+                {uploads.map((upload) => {
+                    return (
+                        <div className="uploadMap">
+                            <div className="upload"><p key={upload.title}>Name: <span style={{fontWeight: "bold"}}>{upload.title}</span></p></div>
+                            <div className="upload"><p>Compatability: <span style={{fontWeight: "bold"}}>{upload.combatability}</span></p></div>
+                            <div className="upload"><p>Type: <span style={{fontWeight: "bold"}}>{upload.type}</span></p></div>
+                            <div className="upload"><p>Walkthrough: <span style={{fontWeight: "bold"}}>{upload.walkthrough}</span></p></div>
+                        </div>
+                        )
+                })}
+            </div>
         </div>
     )
 }
